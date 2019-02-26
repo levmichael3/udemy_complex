81e1b358b7d3438bef35594021afde9b8dc705f9
Michael Lev <michael.lev@automat-it.com>
2019-02-26 11:21:52 +0200

    Initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.POSTGRES_USER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.POSTGRES_DB,
+  pgPassword: process.env.POSTGRES_PASSWORD,
+  pgPort: process.env.PGPORT
+};
